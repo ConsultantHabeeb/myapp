8ae0247522b9b5a7726989f2efdd6ff28ec40079
ConsultantHabeeb <habeeb.dev@outlook.com>
2023-12-23 19:18:04 +0530

    api code added

---
+++ src/app/api/todos/route.ts
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { NextResponse } from "next/server";
+
+const API_URL='https://jsonplaceholder.typicode.com/todos';
+
+export async function GETqqq(){
+
+    const res = await fetch(API_URL)
+
+    const todos =await res.json()
+
+    return NextResponse.json(todos)
+}
+
+export async function GET(){
+
+    const response = await axios.get(`${API_URL}`);
+    return NextResponse.json(response.data);
+}
